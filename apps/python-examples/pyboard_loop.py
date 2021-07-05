0b5b8c0f31d4b2488f15770276b02818aeb763ae
Adam Lukomski <aw.lukomski@gmail.com>
2021-07-05 12:36:45 +0200

    device fallback to MicroPython /dev/input/event when no /dev/input/hidman0

--- apps/python-examples/pyboard_loop.py
+++ apps/python-examples/pyboard_loop.py
@@ -8,10 +8,21 @@ devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 for device in devices:
     print(device.path, device.name, device.phys)
 
-print("Properties of the selected input device...")
-device = evdev.InputDevice('/dev/input/hidman0')
-print(device)
+selected_device = None
+if '/dev/input/hidman0' in evdev.list_devices():
+    selected_device = '/dev/input/hidman0'
+else:
+    for device in devices:
+        if 'MicroPython' in device.name:
+            selected_device = device.path
 
-print("Waiting for input events...")
-for event in device.read_loop():
-    print(evdev.categorize(event))
+if selected_device:
+    print("Properties of the selected input device...")
+    device = evdev.InputDevice(selected_device)
+    print(device)
+
+    print("Waiting for input events...")
+    for event in device.read_loop():
+        print(evdev.categorize(event))
+else:
+    print("No MicroPython device found. Please link a MicroPython board to /dev/input/hidman0")
--- apps/python-examples/pyboard_server.py
+++ apps/python-examples/pyboard_server.py
@@ -1,4 +1,14 @@
 from hidman.core import HIDServer
+import evdev
 
-serv = HIDServer(device='/dev/input/hidman0', address="tcp://*:6666")
+devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+selected_device = None
+if '/dev/input/hidman0' in evdev.list_devices():
+    selected_device = '/dev/input/hidman0'
+else:
+    for device in devices:
+        if 'MicroPython' in device.name:
+            selected_device = device.path
+
+serv = HIDServer(device=selected_device, address="tcp://*:6666")
 serv.run()
