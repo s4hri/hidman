0ac9866543259006f18a571c5bb0a086072b25fb
Davide De Tommaso <dtmdvd@gmail.com>
2022-04-04 15:12:36 +0200

    refactoring v1.0

--- hidman/__init__.py
+++ hidman/__init__.py
@@ -1,6 +1,6 @@
 __authors__ = 'Davide De Tommaso'
 __emails__ = 'davide.detommaso@iit.it'
 __license__ = 'MIT'
-__version__ = '0.9'
+__version__ = '1.0'
 __description__ = 'A Python based HID (Human Interface Device) events manager'
 __requirements__ = ['pytest>=6.2.4', 'evdev>=1.4.0', 'pyzmq>=22.1.0']
